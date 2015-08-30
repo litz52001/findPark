fd68cbe7d9c309b5336f7f2150751e13b49cf991
Litz <Litz@Litz-PC>
2015-08-30 14:26:00 +0800

    测试提交

--- FindParg/src/com/example/findparg/MainActivity.java
+++ FindParg/src/com/example/findparg/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.findparg;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 测试的
+ * @author Litz
+ *
+ */
 public class MainActivity extends Activity {
 
 	@Override
